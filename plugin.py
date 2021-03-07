1de7b05abf684560f6af4a2ddd19483c80179a08
Lucas Alber <lucasd.alber@gmail.com>
2021-03-07 15:05:31 +0100

    Download and install server (#4)

--- plugin.py
+++ plugin.py
@@ -5,12 +5,69 @@ from LSP.plugin import unregister_plugin
 from LSP.plugin.core.typing import Optional, Any, List, Dict, Mapping, Callable
 import os
 import sublime
+from urllib.request import urlopen
+from urllib.error import URLError
+import re
+import shutil
 import tempfile
+import tarfile
 
 # TODO: Not part of the public API :(
 from LSP.plugin.core.edit import apply_workspace_edit
 from LSP.plugin.core.edit import parse_workspace_edit
 from LSP.plugin.core.views import location_to_encoded_filename
+
+
+DOWNLOAD_URL = "http://download.eclipse.org/jdtls/snapshots"
+LATEST_SNAPSHOT = None
+SETTINGS_FILENAME = "LSP-jdtls.sublime-settings"
+STORAGE_DIR = 'LSP-jdtls'
+SERVER_DIR = "server"
+DATA_DIR = "data"
+
+
+def fetch_latest_release() -> None:
+    """
+    Fetches the latest release.
+    """
+    global LATEST_SNAPSHOT
+    if not LATEST_SNAPSHOT:
+        try:
+            with urlopen(DOWNLOAD_URL + "/latest.txt") as f:
+                data = f.read().decode('utf-8')
+                version = re.search("jdt-language-server-(.*).tar.gz", data)
+                LATEST_SNAPSHOT = version.group(1)
+        except URLError:
+            pass
+
+
+def serverversion() -> Optional[str]:
+    """
+    Returns the version of to use. Can be None if
+    no version is set in settings and no connection is available and
+    and no server is available offline.
+    """
+    fetch_latest_release()
+    settings = sublime.load_settings(SETTINGS_FILENAME)
+    version = settings.get('version')
+    if version:
+        return version
+    return LATEST_SNAPSHOT
+
+
+def serverdir(storage_path) -> str:
+    """
+    The directory of the server.
+    """
+    version = serverversion()
+    servers_dir = os.path.join(storage_path, SERVER_DIR)
+    if version:
+        return os.path.join(servers_dir, version)
+    else:
+        servers = os.listdir(servers_dir)
+        if servers:
+            return os.path.join(servers_dir, servers[0])
+    raise ConnectionError("current release could not be fetched and no release is available offline")
 
 
 def _jdtls_platform() -> str:
@@ -25,14 +82,23 @@ def _jdtls_platform() -> str:
         raise ValueError("unknown platform: {}".format(p))
 
 
+def download_file(url, file_name) -> None:
+    with urlopen(url) as response, open(file_name, 'wb') as out_file:
+        shutil.copyfileobj(response, out_file)
+
+
 class EclipseJavaDevelopmentTools(AbstractPlugin):
     @classmethod
     def name(cls) -> str:
         return "jdtls"
 
     @classmethod
+    def storage_subpath(cls) -> str:
+        return os.path.join(cls.storage_path(), STORAGE_DIR)
+
+    @classmethod
     def additional_variables(cls) -> Optional[Dict[str, str]]:
-        settings = sublime.load_settings("LSP-jdtls.sublime-settings")
+        settings = sublime.load_settings(SETTINGS_FILENAME)
         java_home = settings.get("settings").get("java.home")
         if not java_home:
             java_home = os.environ.get("JAVA_HOME")
@@ -40,15 +106,51 @@ class EclipseJavaDevelopmentTools(AbstractPlugin):
             java_executable = os.path.join(java_home, "bin", "java")
         else:
             java_executable = "java"
+
+        launcher_version = ""
+        for file in os.listdir(os.path.join(serverdir(cls.storage_subpath()), "plugins")):
+            match = re.search("org.eclipse.equinox.launcher_(.*).jar", file)
+            if match:
+                launcher_version = match.group(1)
         return {
             "java_executable": java_executable,
             "watch_parent_process": "false" if sublime.platform() == "windows" else "true",
-            "jdtls_platform": _jdtls_platform()
+            "jdtls_platform": _jdtls_platform(),
+            "serverdir": serverdir(cls.storage_subpath()),
+            "datadir": os.path.join(cls.storage_subpath(), DATA_DIR),
+            "launcher_version": launcher_version
         }
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         self.reflist = []  # type: List[str]
+
+    @classmethod
+    def needs_update_or_installation(cls) -> bool:
+        return not os.path.isdir(serverdir(cls.storage_subpath()))
+
+    @classmethod
+    def install_or_update(cls) -> None:
+        version = serverversion()
+        if not version:
+            return
+        basedir = cls.storage_subpath()
+        if os.path.isdir(basedir):
+            shutil.rmtree(basedir)
+        os.makedirs(basedir)
+        with tempfile.TemporaryDirectory() as tempdir:
+            tar_path = os.path.join(tempdir, "server.tar.gz")
+            sublime.status_message("LSP-jdtls: downloading...")
+            download_file(DOWNLOAD_URL + "/jdt-language-server-" + version + ".tar.gz",
+                          tar_path)
+            sublime.status_message("LSP-jdtls: extracting")
+            tar = tarfile.open(tar_path, "r:gz")
+            tar.extractall(tempdir)
+            tar.close()
+            for dir in os.listdir(tempdir):
+                absdir = os.path.join(tempdir, dir)
+                if os.path.isdir(absdir):
+                    shutil.move(absdir, serverdir(cls.storage_subpath()))
 
     def on_pre_server_command(self, command: Mapping[str, Any], done: Callable[[], None]) -> bool:
         session = self.weaksession()

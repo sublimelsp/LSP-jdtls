ad21425d9672907cd21f93482ebbae8ea4f6817c
Rafal Chlodnicki <rchl2k@gmail.com>
2026-04-08 21:52:04 +0200

    revert

--- boot.py
+++ boot.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 def reload_plugin() -> None:
     import sys
+
     for m in list(sys.modules.keys()):
         if m.startswith(str(__package__) + ".") and m != __name__:
             del sys.modules[m]

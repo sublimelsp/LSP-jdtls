0f9020cdc61bb7a1e4fba54231754193cc57b6b2
Raoul Wols <r@primef.actor>
2026-08-24 21:35:58 +0200

    chore: update server version to 1.60 (#74)
    Co-authored-by: Rafał Chłodnicki <rchl2k@gmail.com>

--- modules/constants.py
+++ modules/constants.py
@@ -1,7 +1,7 @@
 # fmt: off
-LOMBOK_VERSION = "1.18.38"
+LOMBOK_VERSION = "1.18.46"
 LOMBOK_URL = "https://repo1.maven.org/maven2/org/projectlombok/lombok/{version}/lombok-{version}.jar"
-JDTLS_VERSION = "1.47.0"
+JDTLS_VERSION = "1.60.0"
 JDTLS_TAR_URL_FILE = "https://download.eclipse.org/jdtls/milestones/{version}/latest.txt"
 JDTLS_URL = "https://download.eclipse.org/jdtls/milestones/{version}/{tar}"
 VSCODE_PLUGINS = {
